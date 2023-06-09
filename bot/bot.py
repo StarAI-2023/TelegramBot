08a3c63ad412eb527180495fe77b95e0b4decbfa
WillDwz <80217986+WillDwz@users.noreply.github.com>
2023-06-09 00:34:36 -0700

    async pinecone query

--- bot/bot.py
+++ bot/bot.py
@@ -303,13 +303,13 @@ async def message_handle(
             previous_conv = [
                 (
                     "preivous conversation with user:",
-                    long_term_memory.similarity_search(user_id, incoming_message),
+                    await long_term_memory.similarity_search(user_id, incoming_message),
                 )
             ]
             celerity_background = [
                 (
                     "you background:",
-                    long_term_memory.similarity_search(
+                    await long_term_memory.similarity_search(
                         config.celebrity_namespace, incoming_message
                     ),
                 )
