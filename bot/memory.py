0ce6dfdea38a6f57072cba58afe79c2fe1c28714
WillDwz <80217986+WillDwz@users.noreply.github.com>
2023-06-17 00:09:20 -0700

    fix reset_dialog

--- bot/memory.py
+++ bot/memory.py
@@ -49,9 +49,9 @@ class Memory:
         if user_id in self.memory:
             message =  self.memory[user_id]["messages"]
             half = len(message)//2
-            self.memory[user_id]["messages"] = message[:half]
+            self.memory[user_id]["messages"] = message[half:]
             self.memory[user_id]["start_time"] = datetime.now()
-            return message[half:]
+            return message[:half]
         else:
             self.create_dialog(user_id=user_id)
 
